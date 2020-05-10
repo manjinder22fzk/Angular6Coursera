2689a34d24f6569587905cf78df18e6a48ad4d27
manjinder22fzk <manjindersinghfzk@gmail.com>
2020-05-10 17:13:52 +0530

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service'
 
 
 

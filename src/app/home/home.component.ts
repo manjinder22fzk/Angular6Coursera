1c72ca7964beb70e7cd216c48d1f9002c04c91e0
manjinder22fzk <manjindersinghfzk@gmail.com>
2020-05-11 14:14:46 +0530

    Promises Part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,9 +21,9 @@ export class HomeComponent implements OnInit {
     private promotionservice: PromotionService,private leaderService:LeaderService) { }
 
   ngOnInit() {
-    this.dish = this.dishservice.getFeaturedDish();
-    this.promotion = this.promotionservice.getFeaturedPromotion();
-    this.featuredLeader=this.leaderService.getFeaturedLeader();
+    this.dishservice.getFeaturedDish().then(dish=>{this.dish = dish});
+    this.promotionservice.getFeaturedPromotion().then(dish=>{this.promotion = dish});
+    this.leaderService.getFeaturedLeader().then(dish=>{this.featuredLeader=dish});
   }
 
 }
